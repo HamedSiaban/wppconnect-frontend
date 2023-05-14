8e640c561fa4cc449929340ae49a83a3e7aefd03
h.siaban <hamed.mjafari@yahoo.com>
2023-05-14 12:34:25 +0330

    change config ip and port

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "http://localhost:21465/api/";
-window.IP_SOCKET_IO = "http://localhost:21465/";
+window.IP_SERVER = "10.0.18.23:21465/api/";
+window.IP_SOCKET_IO = "10.0.18.23:21465/";
